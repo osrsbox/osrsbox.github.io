d1cb7a758897c0ee3c150338b8424cbf573cd2f6
osrsbox <phoil@osrsbox.com>
2018-11-03 14:52:53 +1300

    Added model ID search tool. Also updated rubyzip to version 1.2.2 to fix security vulnerability.

---
+++ tools/model-search/index.md
@@ -0,0 +1,21 @@
+---
+layout: tool
+title: OSRSBOX | Model ID Search for Old School RuneScape
+tool_name: Model ID Search for Old School RuneScape
+tool_desc: Search an exhaustive and up-to-date list of OSRS Model ID numbers, model names and model types
+include_custom_css: 
+- //cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css
+include_custom_script: 
+- //cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js
+- model-search.js
+---
+
+<table id="search-results" class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Type</th>
+    </tr>
+  </thead>
+</table>
---
+++ tools/model-search/model-search.js
@@ -0,0 +1,23 @@
+// Fetch models_summary.json on page load
+// Then generate osrs-models table
+$.getJSON("https://www.osrsbox.com/osrsbox-db/models_summary.json", function(data) {
+    var table = $('#search-results').DataTable({
+        data:  sanitizeData(data),
+        "order": [[ 0, "asc" ]],
+        "iDisplayLength": 50,
+        columns: [
+            { data: 'id' },
+            { data: 'name' },
+            { data: 'type' }
+        ]
+        })  
+});
+
+// Format original models_summary.json structure into array
+function sanitizeData(data) {
+  var d = [];
+  Object.keys(data).forEach(function(key) {
+    d.push(data[key]);
+  });
+  return d;
+}
